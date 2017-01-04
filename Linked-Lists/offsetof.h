7b116d15d1e4003add5f2016e7ca1f9cb6118252
meizhoubao <18032773381@163.com>
2017-01-04 19:10:53 +0800

    Add some comments

--- Linked-Lists/offsetof.h
+++ Linked-Lists/offsetof.h
@@ -4,7 +4,12 @@
 #undef NULL
 #define NULL ((void *)0)
 
+/**
+ * @TYPE: The type of the structure
+ * @MEMBER: The member within the structure
+ */
 #define offsetof(TYPE, MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
 #define offsetofend(TYPE, MEMBER) \
         (offsetof(TYPE, MEMBER) + sizeof(((TYPE *)0)->MEMBER)
+
 #endif
