f1ac34d1ce7342b3bc421607a9d18c2e8441a98b
meizhoubao <18032773381@163.com>
2017-01-04 19:05:48 +0800

    Add offsetofend Macro

--- Linked-Lists/offsetof.h
+++ Linked-Lists/offsetof.h
@@ -5,5 +5,6 @@
 #define NULL ((void *)0)
 
 #define offsetof(TYPE, MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
-
+#define offsetofend(TYPE, MEMBER) \
+        (offsetof(TYPE, MEMBER) + sizeof(((TYPE *)0)->MEMBER)
 #endif
